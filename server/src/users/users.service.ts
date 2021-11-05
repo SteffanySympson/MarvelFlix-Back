07a2bf7204d6d1f543cf90812beb3ebbd6bf7d8e
Steffany Sympson <ssy.marques@gmail.com>
2021-11-04 22:39:28 -0300

    altr Service user

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { UserRole } from './enum/role.enum';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private db: PrismaService) {}
+
+  async create(data: Prisma.UserCreateInput, role: UserRole): Promise<User> {
+    const userExists = await this.db.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (userExists) {
+      throw new ConflictException(
+        'Esse e-mail já consta em nosso Banco de Dados!',
+      );
+    }
+
+    const user = await this.db.user.create({
+      data,
+    });
+
+    return user;
+  }
+}
