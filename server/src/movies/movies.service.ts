73cb2ea104b44cb59fc313fd1a7a6f2e7d7e4f04
Steffany Sympson <ssy.marques@gmail.com>
2021-11-05 23:34:18 -0300

    config service

--- server/src/movies/movies.service.ts
+++ server/src/movies/movies.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { Prisma, Movie } from '@prisma/client';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  constructor(private db: PrismaService) {}
+
+  async create(data: Prisma.MovieCreateInput): Promise<Movie> {
+    const movie = await this.db.movie.create({ data });
+    return movie;
+  }
+}
