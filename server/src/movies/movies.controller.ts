1782d49a5be57dc26efa8a67d8222b018e33d37e
Steffany Sympson <ssy.marques@gmail.com>
2021-11-05 23:34:07 -0300

    Movie Controller

--- server/src/movies/movies.controller.ts
+++ server/src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Body, Post } from '@nestjs/common';
+import { Movie } from '@prisma/client';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { MoviesService } from './movies.service';
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  constructor(private service: MoviesService) {}
+
+  @Post('create')
+  createMovie(@Body() data: CreateMovieDto): Promise<Movie> {
+    return this.service.create(data);
+  }
+}
