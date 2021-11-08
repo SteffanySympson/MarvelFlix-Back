a93c954a9b0771aba7544366741241ac561bb370
Steffany Sympson <ssy.marques@gmail.com>
2021-11-08 17:22:55 -0300

    class validator

--- server/src/movies/dto/create-movie.dto.ts
+++ server/src/movies/dto/create-movie.dto.ts
@@ -1,7 +1,15 @@
+import { IsString, IsNotEmpty, IsUrl } from 'class-validator';
+
 export class CreateMovieDto {
+  @IsString()
   name: string;
+
   year: Date;
   length: Date;
+
+  @IsNotEmpty()
   storyline: string;
+
+  @IsUrl()
   image: string;
 }
