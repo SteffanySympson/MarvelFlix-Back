f68ba21d0644c007373126c6d0ed59387d1064e4
Steffany Sympson <ssy.marques@gmail.com>
2021-11-08 00:25:23 -0300

    deleteOne id

--- server/prisma/migrations/20211108031808_/migration.sql
+++ server/prisma/migrations/20211108031808_/migration.sql
@@ -12,5 +12,19 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "year" TIMESTAMP(3) NOT NULL,
+    "length" TIMESTAMP(3) NOT NULL,
+    "storyline" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
